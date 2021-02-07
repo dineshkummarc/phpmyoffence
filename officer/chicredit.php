a7369ee939356a824b08625520425bc8d558e4b1
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2021-02-07 17:08:27 +0530

    update

--- officer/chicredit.php
+++ officer/chicredit.php
@@ -1,4 +1,3 @@
 <nav class = "navbar-default" id = "footer">
 		<label class = "navbar-brand pull-right">&copy; Traffic offense  system  <?php echo date('Y', strtotime('+8 HOURS'))?></label>
-		<label class = "navbar-brand ">tremendouschatikobo@gmail.com ( contact me for the love of better web)</label>
 	</nav>
